4cbb6b14da62132c7951e5763bfe236a8394476d
Leonardo Cavalli Maran Santos <leocavalli0203@gmail.com>
2025-04-28 22:20:52 -0300

    Update classes.ts

--- classes.ts
+++ classes.ts
@@ -1,22 +1,27 @@
 class Vehicle{
-    drive(): void {
-        console.log('vroom vroom');
-    }
-    honk(): void {
+
+    protected honk(): void {
         console.log('beep beep');
     }
+
+    constructor(public color:string, public wheels:number){
+        this.color = color;
+        this.wheels = wheels;
+        console.log(`This vehicle is ${color} and has ${wheels} wheels`);
+        
+    }
 }
 
-class Car extends   Vehicle{
-    drive(): void {
+class Car extends Vehicle{
+    private drive(): void {
         console.log('vroom vroom, I am a car');
+        this.honk(); // Call the protected method from the parent class
     }
-    honk(): void {
-        console.log('beep beep, I am a car');
+    startDriving(): void {
+        this.drive(); // Call the private method from within the class
     }
 }
 
-const car = new Car(); 
-car.drive(); // vroom vroom
-car.honk(); // beep beep
+const car = new Car('red', 4); 
+car.startDriving(); // vroom vroom
 // The above code defines a class named Vehicle with a method drive that logs 'vroom vroom' to the console.
